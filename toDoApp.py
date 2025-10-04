21562e1e995ab367eea68315184f40c6158e80d4
NUYDA <mknuyda@fit.edu.ph>
2025-10-04 14:25:32 +0800

    fixed main() and showTasks() formatting

--- toDoApp.py
+++ toDoApp.py
@@ -1,39 +1,42 @@
 # toDoApp.py
 
+import os
+
 tasks=[]
 
 def addtask(task) :
   tasks.append(task)
   print("task added!")
 
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+def showTasks():
+    if len(tasks) == 0:
+        print("no tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range(len(tasks)):
+            print(i+1, ".", tasks[i])
 
 def removetask(tasknumber):
     tasks.pop(tasknumber) 
-    print("task removed!!")
+    print("Task Removed!!")
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
+        print("4. Exit")
+        ch = input("Enter Choice: ")
+        if ch == "1":
+            t = input("Enter Task: ")
             addtask(t)
-        elif ch=="2":
+        elif ch == "2":
             showTasks()
-        elif ch=="3":
+        elif ch == "3":
             n=int(input("enter task no to remove: "))
             removetask(n)   
-        elif ch=="4":
-            break;
+        elif ch == "4":
+            break
         else:
             print("wrong choice!!")
+    os.system("pause")
 main()
