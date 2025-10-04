7421e0b8b1d24b422fbdab950bf997bc4a90b10a
JohnJosephCarloNarag <202311582@fit.edu.ph>
2025-10-04 14:43:56 +0800

    added docstrings

--- toDoApp.py
+++ toDoApp.py
@@ -1,43 +1,43 @@
 # toDoApp.py
 
-import os
-
 tasks=[]
 
-def add_task(task) :
-  tasks.append(task)
-  print("task added!")
+def add_task(task):
+    """Task adding function"""
+    tasks.append(task)
+    print("task added!")
 
-def showTasks():
-    if len(tasks) == 0:
-        print("No Tasks Yet")
+def show_tasks( ):
+    """Show all tasks function"""
+    if len(tasks)==0:
+        print("no tasks yet")
     else:
-        for i in range(len(tasks)):
-            print(f"{i+1}. {tasks[i]}")
+        for i in range (len(tasks)):
+            print(i+1,".",tasks[i])
 
 def remove_task(tasknumber):
-    tasks.pop(tasknumber) 
-    print("Task Removed!!")
+    """Task removing Function"""
+    tasks.pop(tasknumber)
+    print("task removed!!")
 
 def main():
+    """The running code"""
     while True:
-        print("1. Add Task")
-        print("2. Show Tasks")
-        print("3. Remove Task")
-        print("4. Exit")
-        ch = input("Enter Choice: ")
-        if ch == "1":
-            t = input("Enter Task: ")
-            addtask(t)
-        elif ch == "2":
-            showTasks()
-        elif ch == "3":
-            n = int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch == "4":
-            break
+        print("1 Add Task")
+        print("2.Show Tasks")
+        print("3.Remove Task")
+        print("4- Exit")
+        ch = input("enter choice : ")
+        if ch=="1":
+            t = input("enter task : ")
+            add_task(t)
+        elif ch=="2":
+            show_tasks()
+        elif ch=="3":
+            n=int(input("enter task no to remove: "))
+            remove_task(n)   
+        elif ch=="4":
+            break;
         else:
             print("wrong choice!!")
-        os.system("pause")
-        os.system("cls")
 main()
